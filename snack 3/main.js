a84d28ed0938ee18f7bd9f9f6c55b5cd81e9adf9
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-05 14:48:59 +0100

    snack 3 done

--- snack 3/main.js
+++ snack 3/main.js
@@ -1,1 +1,13 @@
 console.log("hello world");
+
+console.log("hello world");
+
+const animal = [
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+  ]
+
+  const mammal = animal.filter(animal => animal.classe === "mammiferi")
+
+  console.log(mammal);
