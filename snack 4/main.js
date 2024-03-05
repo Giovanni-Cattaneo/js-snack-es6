11eda8ea4b446f90e90627953717c8c7b6d4814b
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-05 14:57:26 +0100

    done snack 4

--- snack 4/main.js
+++ snack 4/main.js
@@ -1,1 +1,41 @@
 console.log("hello world");
+
+const people = [
+    {
+        nome : "Giovanni",
+        cognome : "Cattaneo",
+        età : 25,
+    },
+    {
+        nome : "Eleonora",
+        cognome : "Cattaneo",
+        età : 29,
+    },
+    {
+        nome : "Emma",
+        cognome : "Cattaneo",
+        età : 15,
+    },
+    {
+        nome : "Luca",
+        cognome : "Cattaneo",
+        età : 60,
+    },
+    {
+        nome : "emanuela",
+        cognome : "Cattaneo",
+        età : 55,
+    },
+]
+
+console.log(people);
+
+people.forEach(person => {
+
+    // console.log(person);
+    if (person.età > 18) {
+        console.log(person.nome, person.cognome, `può guidare in quanto maggiorenne`);
+    } else {
+        console.log(person.nome, person.cognome, `non può guidare in quanto minorenne`); 
+    }
+});
