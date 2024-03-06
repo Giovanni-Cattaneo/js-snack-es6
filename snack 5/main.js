1bdc33b3aa3efd4c4f679e5470a0c0e0934a0044
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-06 16:16:06 +0100

    edit add foreach function

--- snack 5/main.js
+++ snack 5/main.js
@@ -1,1 +1,27 @@
 console.log("Hello world");
+
+// i pesi sono in kilogrammi
+
+const bikes = [
+    {nome : "mountan bike", peso: 5},
+    {nome : "mountan bike", peso: 10},
+    {nome : "mountan bike", peso: 12},
+    {nome : "mountan bike", peso: 8},
+    {nome : "mountan bike", peso: 7},
+]
+
+const [{peso}] = bikes
+
+console.log(peso);
+
+let weigth = 100;
+
+bikes.forEach(function (bike){
+
+    if (peso < weigth ) {
+        weigth = peso
+        console.log(`la bici che pesa meno è una ${bike.nome} e pesa ${weigth} kili`);
+    }
+})
+
+
--- snack 5/readme.md
+++ snack 5/readme.md
@@ -1,3 +1,7 @@
 Creare un array di oggetti:
+- crea una costante array che cntenga moltweplici oggetti
+
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+- ad ogni oggetto dai nome:.... , peso:..... ,
+
 Stampare in console la bici con peso minore utilizzando destructuring e template literal
